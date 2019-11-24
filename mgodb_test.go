5d1c77affec6debc8b5716db67be5fc0f6852050
feige <feige84@126.com>
2019-11-24 22:08:00 +0800

    111

--- mgodb_test.go
+++ mgodb_test.go
@@ -1,32 +1,59 @@
 package mgodb
 
 import (
-	"context"
 	"fmt"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 	"testing"
-	"time"
 )
+
+type Person struct {
+	NAME  string
+	PHONE string
+}
 
 func TestExecute(t *testing.T) {
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://foo:bar@localhost:27017"))
+	session, err := mgo.Dial("127.0.0.1:27017") //连接数据库
 	if err != nil {
 		panic(err)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	db := session.DB("dds")        //数据库名称
+	collection := db.C("dy_aweme") //如果该集合已经存在的话，则直接返回
+	countNum, err := collection.Count()
 	if err != nil {
 		panic(err)
 	}
-	collection := client.Database("dds").Collection("dy_aweme")
-	res, err := collection.InsertOne(context.Background(), bson.M{"hello": "world"})
-	if err != nil {
-		panic(err)
-	}
-	id := res.InsertedID
-	fmt.Println("id:", id)
+	fmt.Println("Things objects count: ", countNum)
+
+	result := []Person{}
+	//普通查询
+	err = collection.Find(bson.M{"name": "zhangzheHero"}).Sort("name").All(&result)
+	fmt.Println(result)
+
+	//一次可以插入多个对象 插入两个Person对象
+	//插入
+	// temp := &Person{
+	//		PHONE: "18811577546",
+	//		NAME:  "zhangzheHero",
+	//	}
+	//err = collection.Insert(&Person{"Ale", "+55 53 8116 9639"}, temp)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//更新
+	//err = collection.Update(bson.M{"name": "zhangzheHero"}, bson.M{"$set": bson.M{"phone": "13988888888"}})
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//删除
+	//err = collection.Remove(bson.M{"name":"Ale"})
+	//if err != nil {
+	//	panic(err)
+	//}
 }
